422de0d4f583f91df644d3b0974676bbcb240f39
proyectosigap <zmamanir@unmsm.edu.pe>
2021-10-30 14:30:02 -0500

    version 1.0

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/delati-amljs17v1-frontend'));
+app.use(express.static('./dist/js17-ml-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/delati-amljs17v1-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/js17-ml-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
